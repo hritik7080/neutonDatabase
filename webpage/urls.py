aa3f3a4dc11e2e47ca71df365c01103e632c1375
hritik7080 <hritikgupta7080@gmail.com>
2021-03-18 03:25:34 +0530

    asd

--- webpage/urls.py
+++ webpage/urls.py
@@ -8,7 +8,8 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
     re_path(r'^track', TemplateView.as_view(template_name='index.html')),
     re_path(r'^course', TemplateView.as_view(template_name='index.html')),
-    path('service-worker.js', TemplateView.as_view(template_name='service-worker.js'))
+    path('service-worker.js', TemplateView.as_view(template_name='service-worker.js')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type="text/plain"))
     
     # re_path(r'^(?P<path>.*)/$', TemplateView.as_view(template_name="index.html")),
 ]
